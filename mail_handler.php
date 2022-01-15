25ed5a5c86c8e8392320446021f40a1a9c4a17bf
@piotr.panek <piotrpanek89@outlook.com>
2022-01-15 19:58:21 +0100

    Simple php email validation added

--- mail_handler.php
+++ mail_handler.php
@@ -2,12 +2,19 @@
 $to      = 'biuro@idealweb.pl';
 $first_name = $_POST['visitor_name'];
 $subject = "Form submission";
+$email = $_POST['visitor_email'];
 $message = $first_name . "  wrote the following:" . "\n\n" . $_POST['visitor_msg'];
 $headers = array(
     'From' => 'biuro@idealweb.pl',
-    'Reply-To' => $_POST['visitor_email'],
+    'Reply-To' => $email,
     'X-Mailer' => 'PHP/' . phpversion()
 );
 
-mail($to, $subject, $message, $headers);
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email is a valid email address");
+    mail($to, $subject, $message, $headers);
+  } else {
+    echo("$email is not a valid email address");
+  }
+
 ?>
